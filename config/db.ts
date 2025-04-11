e673fc4450948ba20862ff701775181827a32a82
ayomide007 <ayomideajayi007@gmail.com>
2025-04-11 11:49:35 +0100

    updated master 4/11

--- config/db.ts
+++ config/db.ts
@@ -1,12 +1,22 @@
 import { Sequelize } from 'sequelize-typescript';
+import { User } from '../models/userModel';
 
+// Create a Sequelize instance using individual properties
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost', // Your host
-  username: 'yourUsername', // Your database username
-  password: 'yourPassword', // Your database password
-  database: 'yourDatabaseName',
+  host: process.env.HOST, // Your host
+  username: process.env.POSTGRESQL_USERNAME, // Your database username
+  password: process.env.POSTGRESQL_PASSWORD, // Your database password
+  database: process.env.DATABASENAME, // Your database name
+  port: Number(process.env.PORT) || 5432, // PostgreSQL default port
   models: [__dirname + '/models'], // Path to your models
+  pool: {
+    max: 10,         // maximum number of connections
+    min: 0,          // minimum number of connections
+    acquire: 30000,  // max time in ms to try getting a connection before throwing error
+    idle: 10000      // time in ms before releasing an idle connection
+  },
+  logging: false
 });
 
 export default sequelize;
